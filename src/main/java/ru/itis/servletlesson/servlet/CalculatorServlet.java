d31c798362bec15b6298e7b97d3fbf49f270ad87
mugur <mugurr@mail.ru>
2025-04-04 22:49:21 +0500

    добавил математические операции

--- src/main/java/ru/itis/servletlesson/servlet/CalculatorServlet.java
+++ src/main/java/ru/itis/servletlesson/servlet/CalculatorServlet.java
@@ -24,13 +24,33 @@ public class CalculatorServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int a = Integer.parseInt(req.getParameter("value1"));
         int b = Integer.parseInt(req.getParameter("value2"));
+        String operator = req.getParameter("operation1");
 
-        int result = calculator.sum(a,b);
+        //int result = calculator.sum(a,b);
+        int result = 0;
+
+        switch (operator) {
+            case "+":
+                result = calculator.sum(a, b);
+                break;
+            case "-":
+                result = calculator.minus(a, b);
+                break;
+            case "*":
+                result = calculator.multiply(a, b);
+                break;
+            case "/":
+                result = calculator.divide(a, b);
+                break;
+        }
+
+
 
         PrintWriter out = resp.getWriter();
 
         out.println("<html><body");
         out.println("<h1> Result: " + result + "</h1>");
+        out.println("<h1> Operation: " + operator + "</h1>");
         out.println("</body></html>");
 
 
